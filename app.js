7489a166ad714acef298deb8e7530c88a88d185a
Justin4aDrive <144923040+Justin4aDrive@users.noreply.github.com>
2023-10-11 09:02:31 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+Class App {
+    constructor(username)  {
+        this.day = "Monday":
+        this.greeting = 'Hi, ${username}';
+    }
+
+    MessageChannel(){
+        const my Message = "This is a message"
+        return MediaKeyMessageEvent;
+    }
+}
